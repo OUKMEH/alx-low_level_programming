7db12530a3b624d9e0411a458f3640855c8c8fdd
Mehdi <oukiramehdi@gmail.com>
2022-12-15 11:36:49 -0800

    55

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include "main.h"
+/**
+ * mul - Entry point
+ * @a: the first parameter
+ * @b: the second parameter
+ *
+ * the function will multiply the two parameters
+ *
+ * Return: the result of multiplication
+ *
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
