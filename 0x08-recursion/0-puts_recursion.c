02a09edbccc328b8c11c24677f91cfde875af937
Mehdi <oukiramehdi@gmail.com>
2023-01-04 14:36:32 -0800

    66

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,8 +6,7 @@
  *
  *
  */
-void _puts_recursion(char *s);
-
+void _puts_recursion(char *s)
 {
 	if (*s)
 	{
@@ -16,5 +15,5 @@ void _puts_recursion(char *s);
 
 	}
 	else
-	_putchar(\n)
+	_putchar('\n');
 }
