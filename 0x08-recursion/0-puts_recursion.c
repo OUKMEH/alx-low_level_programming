8ddd1b94cda23e7fff5f2f44357f687374ba507a
Mehdi <oukiramehdi@gmail.com>
2023-01-04 14:31:24 -0800

    55

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/*
+ *FUNCTION THAT PRINTS A STRING FOLLOWED BY A NEW LINE
+ @S:string
+ *
+ *
+ *
+ */
+void _puts_recursion(char *s);
+
+{
+	if (*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+
+	}
+	else
+	_putchar(\n)
+}
