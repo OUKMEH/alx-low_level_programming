4235995259fc3a209a0bf2e3ce440cb9b56a2d46
Mehdi <oukiramehdi@gmail.com>
2023-02-02 09:31:15 -0800

    555

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,31 +1,35 @@
 #include "main.h"
+
 /**
-* binary_to_uint - convert binary to decimal.
-* @b: the binary number in string format
-* Description: convert binary to decimal
-* section header: the header of this function is header.h
-* Return: this return the convert number.
-*/
+ * binary_to_uint - Converts binary to unsigned int
+ *
+ * @b: Pointer to string of chars
+ *
+ * Return: Converted number
+ */
+
 unsigned int binary_to_uint(const char *b)
 {
-unsigned int result, base;
-int i;
-result = 0;
-i = 0;
-base = 1;
-if (!b)
-return (0);
-while (*(b + i))
-{
-if (*(b + i) != 0 && *(b + i) != 1)
-return (0);
-i++;
-}
-for (i--; i >= 0; i--)
-{
-if (*(b + i) == 1)
-result = result + base;
-base = base * 2;
-}
-return (result);
+	signed int index = 0, num = 0;
+
+
+	if (!b)
+		return (0);
+
+	while (b[index] != '\0')
+	{
+		if (b[index] != '0' && b[index] != '1')
+		{
+			return (0);
+		}
+
+		num <<= 1;
+
+		if (b[index] & 1)
+		{
+			num += 1;
+		}
+		index += 1;
+	}
+	return (num);
 }
--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,27 +1,29 @@
 #include "main.h"
+
 /**
-* print_binary - decimal to binary without use / %
-* @n: the decimal
-* Description: convert decimal to binary
-* section header: the header of this function is holberton.h
-* Return: no return
-*/
+ * print_binary - Prints binary
+ *
+ * @n: Unsigned int
+ *
+ * Return: Converted number
+ */
+
 void print_binary(unsigned long int n)
 {
-int flag = 0;
-unsigned long int mask = 1;
-mask <<= 63;
-if (n == 0)
-_putchar(0);
-while (mask > 0)
-{
-if ((n & mask) == 0 && flag == 1)
-_putchar(0);
-if ((n & mask) != 0)
-{
-_putchar(1);
-flag = 1;
-}
-mask = mask >> 1;
-}
+
+	if (n > 1)
+	{
+	print_binary(n >> 1);
+	}
+
+	if (n & 1)
+	{
+		_putchar('1');
+	}
+	else
+	{
+		_putchar('0');
+	}
+
+
 }
